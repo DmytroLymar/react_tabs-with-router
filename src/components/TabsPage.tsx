a69b7d10a5ca240e44b69f2b5527d640df2b230f
Dmytro Lymar <dmytrolymar901@gmail.com>
2025-11-06 19:22:15 +0200

    feat: replace custom Tabs with react-tabs on TabsPage to satisfy tech requirement

--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames';
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
+import React, { useMemo } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Tabs as RTabs, TabList, Tab } from 'react-tabs';
+import { Tab as TabModel } from '../types/Tab';
 
-const tabs: Tab[] = [
+const tabs: TabModel[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
@@ -11,33 +13,50 @@ const tabs: Tab[] = [
 
 export const TabsPage: React.FC = () => {
   const { tabId } = useParams<{ tabId?: string }>();
-  const activeTab = tabs.find(tab => tab.id === tabId);
+  const navigate = useNavigate();
+  const indexById = useMemo(
+    () => new Map(tabs.map((t, i) => [t.id, i] as const)),
+    [],
+  );
+  const isValid = !!tabId && indexById.has(tabId);
+  const selectedIndex = isValid ? indexById.get(tabId!)! : 0;
+  const activeTab = isValid ? tabs[selectedIndex] : null;
 
   return (
     <div className="section">
       <div className="container">
         <h1 className="title">Tabs page</h1>
 
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => {
-              const isActive = tab.id === tabId;
+        <RTabs
+          selectedIndex={selectedIndex}
+          onSelect={i => {
+            navigate(`/tabs/${tabs[i].id}`);
 
-              return (
-                <li
-                  key={tab.id}
-                  data-cy="Tab"
-                  className={classNames({ 'is-active': isActive })}
-                >
-                  <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+            return true;
+          }}
+        >
+          <div className="tabs is-boxed">
+            <TabList>
+              {tabs.map(tab => {
+                const isActive = tab.id === tabId;
+
+                return (
+                  <Tab
+                    key={tab.id}
+                    data-cy="Tab"
+                    className={classNames({ 'is-active': isActive })}
+                  >
+                    <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+                  </Tab>
+                );
+              })}
+            </TabList>
+          </div>
+        </RTabs>
+        {/*  */}
 
         <div className="block" data-cy="TabContent">
-          {!tabId || !activeTab ? 'Please select a tab' : activeTab.content}
+          {activeTab ? activeTab.content : 'Please select a tab'}
         </div>
       </div>
     </div>
